3f3b08d811c98b11c3528f689c056bf653a2a14e
Yadnyawalk Tale <ytale@redhat.com>
2017-11-22 13:05:51 +0530

    added php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<head>
+	<title>Resposible disclosure</title>
+	<meta http-equiv="refresh" content="5"/>
+</head>
+
+<body>
+	<h1 align="center">Test page</h1>
+	<h3 align="center">Hello, this is resposible disclosure. Your <span id="span1"></span> subdomain takeovered by <a href="http://twitter.com/yvtale">Yadnyawalkya Tale</a>.
+		<br/>I have contacted your support team, request you to contact yadnyawalkyatale@gmail.com for further information.
+	</h3>
+	<br/>
+	<p align="center">
+		<img align="justify" src="thinkinfosec.png"  height="400"></img>
+		<br/>
+
+		<h3 align="center">I love pressing F5. It's so refreshing..</h3>
+	</p>
+	<script>
+	window.onload = function(){
+    		document.getElementById('span1').innerHTML = window.location;
+	}
+	</script>
+</body>
+
+</html>
